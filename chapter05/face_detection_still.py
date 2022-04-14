72c2c62b5ac4cf62c42301747477de684f02150d
Joe Howse <josephhowse@nummist.com>
2022-04-14 02:02:20 -0300

    Changing rect color for better visibility in print

--- chapter05/face_detection_still.py
+++ chapter05/face_detection_still.py
@@ -7,8 +7,8 @@ img = cv2.imread('../images/woodcutters.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.08, 5)
 for (x, y, w, h) in faces:
-    img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
   
 cv2.imshow('Woodcutters Detected!', img)
-cv2.imwrite('./woodcutters_detected.jpg', img)
+cv2.imwrite('./woodcutters_detected.png', img)
 cv2.waitKey(0)
