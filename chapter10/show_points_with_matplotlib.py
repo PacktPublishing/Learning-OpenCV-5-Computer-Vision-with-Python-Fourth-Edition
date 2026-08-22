a8c7faccbb48259f6a7a49ae9600cec49940ac81
Joe Howse <josephhowse@nummist.com>
2026-08-22 10:19:14 -0300

    Update to Chapter 10 for latest OpenCV loadPointCloud function

--- chapter10/show_points_with_matplotlib.py
+++ chapter10/show_points_with_matplotlib.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 model_path = '../3d_models/jburkardt/street_lamp.ply'
 
-vertices = cv2.loadPointCloud(model_path)[0]
+vertices, normals, rgb = cv2.loadPointCloud(model_path)
 vertices = vertices.squeeze(axis=1)
 
 
